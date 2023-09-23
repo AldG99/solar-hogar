e372d793c6c59b802b03347781d087b304691690
Alfredo <garciaglz17@outlook.es>
2023-09-22 18:23:47 -0600

    Editar Carousel

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css'; // Importa los estilos CSS
-import { FaFacebook, FaInstagram, FaTwitter, FaGooglePlus } from "react-icons/fa";
+import { FaFacebook, FaInstagram, FaTwitter, FaGooglePlus } from 'react-icons/fa';
 import './styles/scrollbar.scss';
 import './App.scss'; // Asegúrate de importar tu archivo SCSS
 
@@ -10,42 +10,51 @@ import './App.scss'; // Asegúrate de importar tu archivo SCSS
 function App() {
 
   return (
-<div className="App">
+<div className='App'>
   {/* Header */}
-  <header className="header">
+  <header className='header'>
     {/* Barra de navegación */}
-    <nav className="navbar">
-      <div className="container">
+    <nav className='navbar'>
+      <div className='container'>
         {/* Menú de navegación */}
-        <ul className="nav-list">
-          <li><a href="#acerca">Acerca de</a></li>
-          <li><a href="#servicios">Servicios</a></li>
-          <li><a href="#mision">Misión</a></li>
-          <li><a href="#financiacion">Financiación</a></li>
-          <li><a href="#resenas">Reseñas</a></li>
-          <li><a href="#contactenos">Contáctenos</a></li>
+        <ul className='nav-list'>
+          <li><a href='#acerca'>Acerca de</a></li>
+          <li><a href='#servicios'>Servicios</a></li>
+          <li><a href='#mision'>Misión</a></li>
+          <li><a href='#financiacion'>Financiación</a></li>
+          <li><a href='#resenas'>Reseñas</a></li>
+          <li><a href='#contactenos'>Contáctenos</a></li>
         </ul>
       </div>
     </nav>
   </header>
 
   {/* Hero Section */}
-  <section className="hero">
-    <div className="container">
+  <section className='hero'>
+    <div className='container'>
+    <div className='logo-container'>
+    <div className='logotipo'>
+        <img 
+        src={require('./images/logo-panel-solar.png')} 
+        alt='Logo de la empresa' 
+        onContextMenu={(e) => e.preventDefault()}
+        onDragStart={(e) => e.preventDefault()}/>
+      </div>
+      </div>
       {/* Aquí puedes colocar la imagen */}
       <img
         src={require('./images/solar-home-1.jpg')}
         alt='Una Amiga y Un Amigo'
         onContextMenu={(e) => e.preventDefault()}
         onDragStart={(e) => e.preventDefault()}
-        className="hero-image" // Agrega una clase para aplicar estilos específicos
+        className='hero-image' // Agrega una clase para aplicar estilos específicos
       />
-      <div className="hero-content">
+      <div className='hero-content'>
         {/* Aquí puedes colocar el encabezado */}
         <h1>Descubre la Energía del Mañana</h1>
-        <p>Explora nuestra gama de paneles solares y únete a la revolución de la energía sostenible.</p>
-        <button type="button">
-  <a href="#acerca">Aprende Más</a>
+        <p>Únete a la revolución de la energía sostenible.</p>
+        <button type='button'>
+  <a href='#acerca' style={{ textDecoration: 'none' }}>Aprende Más</a>
 </button>
 
       </div>
@@ -54,9 +63,9 @@ function App() {
 
 
 
-  <section className="about" id="acerca">
-  <div className="container">
-    <div className="about-content">
+  <section className='about' id='acerca'>
+  <div className='container'>
+    <div className='about-content'>
       <h2>Acerca de Solar Home</h2>
       <p>
         Solar Home es tu socio confiable en la instalación de sistemas de energía solar residencial
@@ -73,49 +82,49 @@ function App() {
 </section>
 
       {/* Sección de Servicios o Productos */}
-<section className="services" id="servicios">
-  <div className="container">
-    <div className="services-content">
-      <div className="service-item-1">
+<section className='services' id='servicios'>
+  <div className='container'>
+    <div className='services-content'>
+      <div className='service-item-1'>
       <img
         src={require('./images/servicio-1.jpg')}
         alt='Una Amiga y Un Amigo'
         onContextMenu={(e) => e.preventDefault()}
         onDragStart={(e) => e.preventDefault()}
-        className="hero-image" // Agrega una clase para aplicar estilos específicos
+        className='hero-image' // Agrega una clase para aplicar estilos específicos
       />
         <div className='service-text-1'>
-        <h3>Servicio 1</h3>
-        <h4>Servicio 1</h4>
-        <p>Descripción del Servicio 1.</p>
+        <h3>Instalación Profesional</h3>
+        <h4>Energía Sostenible a tu Alcance</h4>
+        <p>Nuestro equipo de expertos en energía solar se encargará de instalar paneles solares de alta eficiencia en tu hogar o empresa. Con una instalación profesional, puedes aprovechar al máximo la energía solar y reducir tus facturas de electricidad.</p>
         </div>
       </div>
-      <div className="service-item-2">
+      <div className='service-item-2'>
       <img
         src={require('./images/servicio-2.jpg')}
         alt='Una Amiga y Un Amigo'
         onContextMenu={(e) => e.preventDefault()}
         onDragStart={(e) => e.preventDefault()}
-        className="hero-image" // Agrega una clase para aplicar estilos específicos
+        className='hero-image' // Agrega una clase para aplicar estilos específicos
       />
         <div className='service-text-2'>
-        <h3>Servicio 2</h3>
-        <h4>Servicio 1</h4>
-        <p>Descripción del Servicio 2.</p>
+        <h3>Mantenimiento Confiable</h3>
+        <h4>Paneles Solares Siempre en Óptimas Condiciones</h4>
+        <p>Ofrecemos servicios de mantenimiento preventivo y reparación de paneles solares. Nuestros técnicos altamente capacitados garantizan que tus paneles solares funcionen de manera eficiente durante años. Mantén tu inversión en energía solar en perfecto estado.</p>
         </div>
       </div>
-      <div className="service-item-3">
+      <div className='service-item-3'>
       <img
         src={require('./images/servicio-3.jpg')}
         alt='Una Amiga y Un Amigo'
         onContextMenu={(e) => e.preventDefault()}
         onDragStart={(e) => e.preventDefault()}
-        className="hero-image" // Agrega una clase para aplicar estilos específicos
+        className='hero-image' // Agrega una clase para aplicar estilos específicos
       />
       <div className='service-text-3'>
-        <h3>Servicio 3</h3>
-        <h4>Servicio 1</h4>
-        <p>Descripción del Servicio 3.</p>
+        <h3>Consultoría Especializada</h3>
+        <h4>Planifica tu Transición a la Energía Solar</h4>
+        <p>Nuestros consultores en energía solar te ayudarán a tomar decisiones informadas sobre la transición a la energía solar. Te guiaremos a través de las opciones de financiamiento, el diseño de sistemas y los incentivos disponibles para que puedas obtener los máximos beneficios de la energía solar.</p>
         </div>
       </div>
     </div>
@@ -124,29 +133,29 @@ function App() {
 
 
       {/* Misión Section */}
-      <section className="mission" id="mision">
-        <div className="container">
+      <section className='mission' id='mision'>
+        <div className='container'>
         <img
         src={require('./images/solar-home-2.jpg')}
         alt='Una Amiga y Un Amigo'
         onContextMenu={(e) => e.preventDefault()}
         onDragStart={(e) => e.preventDefault()}
-        className="hero-image" // Agrega una clase para aplicar estilos específicos
+        className='hero-image' // Agrega una clase para aplicar estilos específicos
       />
       <div className='mission-text'>
-        <h3>Mision</h3>
-        <p>TEXTO</p>
+        <h3>Empoderando Tu Futuro con Energía Solar Sostenible</h3>
+        <p>Nuestra misión es hacer que la energía solar sea accesible y asequible para todos, promoviendo un futuro más limpio y sostenible a través de soluciones de alta calidad y prácticas ambientalmente responsables.</p>
         </div>
         </div>
       </section>
 
-      <section className="financing" id="financiacion">
-  <div className="container">
-    {/* Contenido de la sección "Financiación" */}
-    <div className="video-container">
+      <section className='financing' id='financiacion'>
+  <div className='container'>
+    {/* Contenido de la sección 'Financiación' */}
+    <div className='video-container'>
     <iframe
-  width='800'
-  height='450'
+  width={800 * 0.8} // Recudir tamaño original al 20%
+  height={450 * 0.8}
   src='https://www.youtube.com/embed/xKxrkht7CpY' // URL de embebido del video
   title='Video Solar Home'
   allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
@@ -154,58 +163,62 @@ function App() {
 ></iframe>
 
 </div>
-    <div className="text-container">
+    <div className='text-container'>
       {/* Aquí puedes agregar tu contenido de texto */}
-      <h3>Título del Video</h3>
-      <p>Descripción del video y otros detalles relacionados con la financiación.</p>
+      <h3>Financiación Flexible para un Futuro Sostenible</h3>
+      <p>En Solar Hogar, entendemos que dar el salto hacia la energía solar puede ser una inversión significativa. Por eso, ofrecemos soluciones de financiación flexibles diseñadas para hacer que la transición a la energía solar sea accesible para todos. Nuestros planes de financiamiento te permiten disfrutar de los beneficios de la energía solar sin un impacto inmediato en tu presupuesto. Con tasas competitivas y plazos flexibles, estamos comprometidos en ayudarte a impulsar un futuro sostenible sin preocupaciones financieras. Juntos, podemos hacer que la energía solar sea una realidad para tu hogar o negocio.</p>
     </div>
   </div>
 </section>
 
 
-<section className="reviews" id="resenas">
-  <div className="container">
+<section className='reviews' id='resenas'>
+  <div className='container'>
   <Carousel autoPlay={true} interval={3000} showThumbs={false} showStatus={false} infiniteLoop={true} className='carousel-container'>
   <div style={{ position: 'relative', userSelect: 'none' }}>
         <img src={require('./images/review-1.jpg')} alt='Almacén con Paneles Solares' />
-        <h3>Autor 1</h3>
-        <p>Reseña</p>
+        <h3>María González</h3>
+        <p>¡Los paneles solares han cambiado mi vida! Gracias a ellos, he reducido significativamente mis facturas de electricidad y contribuido a la protección del medio ambiente. ¡Una inversión que realmente vale la pena!</p>
       </div>
       <div style={{ position: 'relative', userSelect: 'none' }}>
         <img src={require('./images/review-2.jpg')} alt='Departamentos con Paneles Solares' />
-        <h3>Autor 2</h3>
-        <p>Reseña</p>
+        <h3>Juan Carlos Pérez</h3>
+        <p>Desde que instalé paneles solares en mi hogar, me he sentido más independiente energéticamente. La tecnología es confiable y eficiente, y no puedo dejar de recomendarla a amigos y familiares.</p>
       </div>
       <div style={{ position: 'relative', userSelect: 'none' }}>
         <img src={require('./images/review-3.jpg')} alt='Casa con Paneles Solares' />
-        <h3>Autor 3</h3>
-        <p>Reseña</p>
+        <h3>Laura Rodríguez</h3>
+        <p>Como dueña de un negocio, la decisión de adoptar paneles solares fue una de las mejores que he tomado. No solo hemos reducido nuestros costos operativos, sino que también hemos ganado la admiración de nuestros clientes por nuestro compromiso con la sostenibilidad.</p>
       </div>
       <div style={{ position: 'relative', userSelect: 'none' }}>
         <img src={require('./images/review-4.jpg')} alt='Campo con Paneles Solares' />
-        <h3>Autor 4</h3>
-        <p>Reseña</p>
+        <h3>Diego López</h3>
+        <p>La energía solar es el futuro, y no puedo estar más satisfecho con mi sistema de paneles solares. Además de los beneficios económicos, siento que estoy haciendo mi parte para combatir el cambio climático. ¡Es una victoria para todos!</p>
       </div>
     </Carousel>
   </div>
 </section>
 
 
-<section className="contact" id="contactenos">
-  <div className="container">
+<section className='contact' id='contactenos'>
+  <div className='container'>
   {/* Agrega los iconos de redes sociales */}
   <div className='container-icons'>
-  <div className="social-icons">
-          <a href="https://github.com/" className="social-icon">
+  <h2>Asegure el futuro energético de su hogar</h2>
+  <p>¡Descubra soluciones confiables de energía de respaldo ahora!</p>
+  <p>+1 (123) 456-7890.</p>
+        <p>info@solar-hogar.com.</p>
+  <div className='social-icons'>
+          <a href='https://github.com/AldG99/solar-hogar' className='social-icon'>
             <FaFacebook />
           </a>
-          <a href="https://github.com/" className="social-icon">
+          <a href='https://github.com/AldG99/solar-hogar' className='social-icon'>
             <FaInstagram />
           </a>
-          <a href="https://github.com/" className="social-icon">
+          <a href='https://github.com/AldG99/solar-hogar' className='social-icon'>
             <FaTwitter />
           </a>
-          <a href="https://github.com/" className="social-icon">
+          <a href='https://github.com/AldG99/solar-hogar' className='social-icon'>
             <FaGooglePlus />
           </a>
         </div>
@@ -214,27 +227,27 @@ function App() {
     <div className='container-form'>
     <form>
       <h2>Contacto</h2>
-      <div className="form-group">
-        <label className='form-text' htmlFor="nombreCompleto">Nombre Completo</label>
-        <input type="text" id="nombreCompleto" name="nombreCompleto" required />
+      <div className='form-group'>
+        <label className='form-text' htmlFor='nombreCompleto'>Nombre Completo</label>
+        <input type='text' id='nombreCompleto' name='nombreCompleto' required />
       </div>
-      <div className="form-group">
-        <label className='form-text' htmlFor="correoElectronico">Correo Electrónico</label>
-        <input type="email" id="correoElectronico" name="correoElectronico" required />
+      <div className='form-group'>
+        <label className='form-text' htmlFor='correoElectronico'>Correo Electrónico</label>
+        <input type='email' id='correoElectronico' name='correoElectronico' required />
       </div>
-      <div className="form-group">
-        <label className='form-text' htmlFor="numeroTelefono">Número de Teléfono</label>
-        <input type="tel" id="numeroTelefono" name="numeroTelefono" required />
+      <div className='form-group'>
+        <label className='form-text' htmlFor='numeroTelefono'>Número de Teléfono</label>
+        <input type='tel' id='numeroTelefono' name='numeroTelefono' required />
       </div>
-      <div className="form-group">
-        <label className='form-text' htmlFor="direccion">Dirección</label>
-        <input type="text" id="direccion" name="direccion" required />
+      <div className='form-group'>
+        <label className='form-text' htmlFor='direccion'>Dirección</label>
+        <input type='text' id='direccion' name='direccion' required />
       </div>
-      <div className="form-group">
-        <label className='form-text' htmlFor="consumoAnual">Consumo Anual de Electricidad (kWh)</label>
-        <input type="number" id="consumoAnual" name="consumoAnual" required />
+      <div className='form-group'>
+        <label className='form-text' htmlFor='consumoAnual'>Consumo Anual de Electricidad (kWh)</label>
+        <input type='number' id='consumoAnual' name='consumoAnual' required />
       </div>
-      <button type="submit">Enviar</button>
+      <button type='submit'>Enviar</button>
     </form>
     </div>
   </div>
@@ -242,8 +255,8 @@ function App() {
 
 
       {/* Footer */}
-      <footer className="footer">
-        <div className="container">
+      <footer className='footer'>
+        <div className='container'>
         <div className='copyright'>
           © 2023 Alfredo García. Todos los derechos reservados.
         </div>
